db9ded0f8848be7df8b3a3f3cf90afbdf4fb2a21
ArmandoVinicius <armando.vinicius12@gmail.com>
2020-08-25 20:02:22 -0300

    saved

---
+++ Minhas respostas/secao-12/examples.js
@@ -0,0 +1,19 @@
+/* let a = 1;
+let b = 2;
+
+if(a == 1){
+    a = b + 2;
+}
+
+console.log(a);
+
+for(i = 0; i < b; i++){
+    a = a + 2;
+    console.log(a);
+}
+
+if(a > 10){
+    a == 25;
+} */
+//Console.log e a primeira forma de debug :D
+
